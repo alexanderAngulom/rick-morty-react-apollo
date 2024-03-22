93b9e8515011b2b6e0118555f132dbfe25f82014
Jorge Alexander Angulo <jalexander9609@gmail.com>
2024-03-21 21:41:20 -0500

    cambios en interfaz para el boton, animaciones

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,67 +1,98 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom'; // Importa Link de react-router-dom
 import { useQuery } from 'react-apollo';
 import { Card, CardMedia, CardContent, Typography, Button, Zoom, makeStyles } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
-import './CharacterDetails.css'; 
-const {  typeDefsById } = require('../typeDefs/typeDefs');
+import './CharacterDetails.css';
+const { typeDefsById } = require('../typeDefs/typeDefs');
 
 const useStyles = makeStyles((theme) => ({
   card: {
     width: 300,
     margin: 'auto',
-    marginTop: theme.spacing(10), 
-    opacity: 1,
+    marginTop: theme.spacing(10),
+    opacity: 0.7,
     transition: 'opacity 0.3s ease',
     '&:hover': {
-      opacity: 1.6,
+      opacity: 1,
     },
+  },
+  progress: {
+    '--progress': '80%',
+    width: 500,
+    height: 50,
+    margin: '9em auto',
+    border: '1px solid #fff',
+    padding: '12px 10px',
+    boxShadow: '0 0 10px #aaa',
+  },
+  bar: {
+    width: 'var(--progress)',
+    height: '100%',
+    background: 'linear-gradient(#18B518, #DCDC3C, #18B518)',
+    backgroundRepeat: 'repeat',
+    boxShadow: '0 0 10px 0px orange',
+    animation: 'shine 4s ease-in infinite, end 1s ease-out 1 7s',
+    transition: 'width 3s ease 3s',
+  },
+  backButton: { 
+    color: '#000000',
+    textDecoration: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing(2),
   },
 }));
 
 const CharacterDetails = () => {
   const { id } = useParams();
-
   const { loading, error, data } = useQuery(typeDefsById, {
     variables: { id },
   });
   const classes = useStyles();
 
-
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className={classes.progress}>
+      <div className={classes.bar}></div>
+      <div>LOADING...</div>
+    </div>
+  ); 
   if (error) return <p>Error</p>;
 
   const { character } = data;
 
   return (
     <Zoom in={true}>
-      <Card className={classes.card}>
-        <CardMedia
-          component="img"
-          alt={character.name}
-          height="300"
-          width="200"
-          image={character.image}
-          title={character.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {character.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Species: {character.species}
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<FavoriteIcon />}
-            style={{ marginTop: '10px' }}
-          >
-            Add to Favorites
-          </Button>
-        </CardContent>
-      </Card>
+      <div>
+        <Link to="/" className={classes.backButton}>Back</Link> {}
+        <Card className={classes.card}>
+          <CardMedia
+            component="img"
+            alt={character.name}
+            height="300"
+            width="200"
+            image={character.image}
+            title={character.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {character.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Species: {character.species}
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<FavoriteIcon />}
+              style={{ marginTop: '10px' }}
+            >
+              Add to Favorites
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
     </Zoom>
   );
 };
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { Grid, Card, CardActionArea, CardContent, CardMedia, Typography, Zoom, Avatar } from '@material-ui/core';
+import { Grid, Card, CardActionArea, CardContent, CardMedia, Typography, Zoom, Avatar, makeStyles} from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import AdbIcon from '@material-ui/icons/Adb';
 
 import { Link } from 'react-router-dom';
 import useCharacterQuery from '../Hooks/querys.js';
 
+const useStyles = makeStyles((theme) => ({
+  
+  progress: {
+    '--progress': '60%',
+    width: 500,
+    height: 50,
+    margin: '9em auto',
+    border: '1px solid #fff',
+    padding: '12px 10px',
+    boxShadow: '0 0 10px #aaa',
+  },
+  bar: {
+    width: 'var(--progress)',
+    height: '100%',
+    background: 'linear-gradient(#18B518, #DCDC3C, #18B518)',
+    backgroundRepeat: 'repeat',
+    boxShadow: '0 0 10px 0px Green',
+    animation: 'shine 4s ease-in infinite, end 1s ease-out 1 7s',
+    transition: 'width 3s ease 3s',
+  },
+}));
 const CharacterList = () => {
   const { loading, error, data } = useCharacterQuery();
+  const classes = useStyles();
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return (
+    <div className={classes.progress}>
+      <div className={classes.bar}></div>
+      <div>LOADING...</div>
+    </div>
+  );  if (error) return <p>Error :(</p>;
 
   if (!data || data.length === 0) return <p>No data available.</p>;
 
